f93b583018e3397c4e84821aa9b4ace95b0d304c
jamesjr19 <ruwartja@gmail.com>
2021-01-12 21:07:12 -0500

    Unity2D-CastawayBlockBreaker initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // config params
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 1;
+    [SerializeField] float yPush = 1;
+    [SerializeField] AudioClip[] ballSounds;
+    [SerializeField] float randomFactor = 0.2f;
+
+    // state
+    Vector2 paddleToBallVector;
+    bool hasStarted = false;
+
+    // Cached component references
+    AudioSource myAudioSource;
+    Rigidbody2D MyRigidBody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
+        MyRigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            MyRigidBody2D.velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector2 velocityTweak = new Vector2
+            (Random.Range(0f, randomFactor), 
+            Random.Range(0f, randomFactor));
+
+        if (hasStarted)
+        {
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            myAudioSource.PlayOneShot(clip);
+            MyRigidBody2D.velocity += velocityTweak;
+        }
+    }
+}
---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Block : MonoBehaviour
+{
+    // config params
+    [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject blockSparklesVFX;
+    [SerializeField] Sprite[] hitSprites;
+    
+    //cahched reference
+    Level level;
+
+    // State vairables
+    [SerializeField] int timesHit;
+
+    private void Start()
+    {
+        CountBreakableBlocks();
+    }
+
+    private void CountBreakableBlocks()
+    {
+        level = FindObjectOfType<Level>();
+        if (tag == "Breakable")
+        {
+            level.CountBlocks();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (tag == "Breakable")
+        {
+            HandleHit();
+        }
+    }
+
+    private void HandleHit()
+    {
+        timesHit++;
+        int maxHits = hitSprites.Length + 1;
+        if (timesHit >= maxHits)
+        {
+            DestroyBlock();
+        }
+        else
+        {
+            ShowNextHitSprite();
+        }
+    }
+
+    private void ShowNextHitSprite()
+    {
+        int spriteIndex = timesHit - 1;
+        if (hitSprites[spriteIndex] != null)
+        {
+            GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        }
+        else
+        {
+            Debug.LogError("Block sprite missing" + gameObject.name);
+        }
+    }
+
+    private void DestroyBlock()
+    {
+        //add points to score and play sound
+        PlayBlockDestroySFX();
+
+        //detroy block and trigger visual fx 
+        Destroy(gameObject);
+        level.BlockDestroyed();
+        TriggerSparklesVFX();
+    }
+
+    private void PlayBlockDestroySFX()
+    {
+        FindObjectOfType<GameSession>().AddToScore();
+
+        //play audio at location of main camera
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+    }
+
+    private void TriggerSparklesVFX()
+    {
+        GameObject sparkles = Instantiate(blockSparklesVFX, transform.position, transform.rotation);
+        Destroy(sparkles, 1f);
+    }
+}
